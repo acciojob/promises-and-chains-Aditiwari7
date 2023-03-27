31daa3b2bb995786973bcb07a50e7f3fa4228561
Aditi Tiwari <115400068+Aditiwari7@users.noreply.github.com>
2023-03-27 20:44:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,23 +2,24 @@
 
 let btn = document.getElementById("btn");
 
-btn.addEventListener("click",  (e)=>{
+btn.addEventListener("click", (e)=>{
 	let age = document.getElementById("age").value;
 	let name = document.getElementById("name").value;
-	return new Promise((res, rej)=>{
+	return new Promise((res, rej)=>{		
 		setTimeout(()=>{
 			console.log("timeout");
-			if(age > 18)
-				res(`Welcome, ${name}. You can vote.`);
-			else
-				rej(`Oh sorry ${name}. You aren't old enough`);
-		},  4000);
-	})
-	.then(mes=>{
-		alert(mes);
-	});
-
-	.catch(err=>{
-		alert(err);
-	});
+			if(age > 18)	
+				
+			      res(`Welcome, ${name}. You can vote.`); 				
+		   else			
+			      rej(`Oh sorry ${name}. You aren't old enough`);	
+	}, 4000) ;  
+	       })
+		.then(mes=>{			
+	           alert(mes);	
+			  
+         })
+		.catch(err=>{
+	         alert(err);
+            });		
 })
